b1abd33a3b9ef9555fbe1abf20b4db040cbd7a1f
Ryan <627311610@qq.com>
2022-04-09 21:28:40 +0800

    fix: netify deploy

--- src/main.ts
+++ src/main.ts
@@ -9,12 +9,7 @@ import 'virtual:windi.css';
 
 const app = createApp(App);
 
-/**
- * router
- *
- * github pages 配置基础路径
- * https://stackoverflow.com/questions/50175802/deploy-vue-to-github-pages-error-with-vue-router/
- */
-app.use(createRouter(createWebHistory('/vue-vite-starter/')));
+// router
+app.use(createRouter(createWebHistory()));
 
 app.mount('#app');
--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,10 @@ import Vue from '@vitejs/plugin-vue';
 import WindiCSS from 'vite-plugin-windicss';
 import { SvgLoader } from './src/utils/plugins';
 
-const isProd = process.env.NODE_ENV === 'production';
+// const isProd = process.env.NODE_ENV === 'production';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // 按需设置，此处为部署到gh-pages
-  base: isProd ? '/vue-vite-starter/' : '/',
   resolve: {
     alias: {
       '@': `${path.resolve(__dirname, 'src')}`
